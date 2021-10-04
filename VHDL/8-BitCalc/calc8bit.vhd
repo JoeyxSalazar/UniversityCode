779ff9b9c319e6bdea67ac1433f36cf4f6bc4eea
JoeyxSalazar <74478647+JoeyxSalazar@users.noreply.github.com>
2021-10-04 16:46:54 -0400

    Update calc8bit.vhd

--- VHDL/8-BitCalc/calc8bit.vhd
+++ VHDL/8-BitCalc/calc8bit.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_signed.all;
 use ieee.std_logic_1164.all;
 
-entity Calc8bit is 
+entity calculator8bit is 
 port   (Din1 : in std_logic_vector (7 downto 0);
 	Din2 : in std_logic_vector (7 downto 0);
        clk: in std_logic;
@@ -13,9 +13,9 @@ port   (Din1 : in std_logic_vector (7 downto 0);
        overflow: out std_logic);
 end entity;
 
-Architecture Arch8 of Calc8bit is 
+Architecture Arch8 of calculator8bit is 
 
-       component reg8bit
+       component register8
        port (Inp: in std_logic_vector(7 downto 0);
               Clk, Load: in std_logic;
               Outp: out std_logic_vector(7 downto 0));
@@ -28,13 +28,32 @@ Architecture Arch8 of Calc8bit is
 		sign: out std_logic;
               Result: out std_logic_vector (7 downto 0));
        end component;
+		 
+		 
+			component Display is
+			port(
+			Input: in std_logic_vector(3 downto 0); --input from calc
+			segmentSeven : out std_logic_vector(6 downto 0)); --7 bit output end Display_Ckt;
+			end component;
+			
+			component DisplaySign is
+			port( Input: in bit; --input from sign
+			segmentSeven : out std_logic_vector(6 downto 0)); --7 bit output end Display_Ckt;
+			end component;
        
 	signal Xout : std_logic_vector (7 downto 0);
 	signal Yout : std_logic_vector (7 downto 0);
+	signal DisL : std_logic_vector (6 downto 0);
+	signal DisR : std_logic_vector (6 downto 0);
+	signal DiSign: std_logic_vector (6 downto 0);
 
 	begin
-		R1 : reg8bit port map (Din1, load(0),clk, Xout);
-		R2 : reg8bit port map (Din2, load(1), clk, Yout);
+		R1 : register8 port map (Din1, load(0),clk, Xout);
+		R2 : register8 port map (Din2, load(1), clk, Yout);
 		Calc : calcprocessing port map(Xout, Yout, opcode, overflow, sign, result);
+		ledL: Display port map (Result(7 downto 4), DisL);
+		ledR: Display port map (Result(3 downto 0), DisR);
+		sign: DisplaySign port map (sign, DiSign);
+		
 
 end Arch8;
